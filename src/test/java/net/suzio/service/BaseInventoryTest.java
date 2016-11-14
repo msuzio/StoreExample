1d1eb03ab58cf542d3ebb071bc4e5a78b9dcc350
Michael Suzio <msuzio@gmail.com>
2016-11-13 19:26:32 -0500

    removed defunct service package

--- src/test/java/net/suzio/service/BaseInventoryTest.java
+++
@@ -1,8 +0,0 @@
-package net.suzio.service;
-
-/** Base class for future tests of Inventory implementations
- * Just a stub right now
- * Created by Michael on 11/12/2016.
- */
-public abstract class BaseInventoryTest {
-}
